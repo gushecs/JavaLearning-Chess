a18e45dbff99c39e89be0fd2a905ddb9af0a9e93
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-07 14:05:04 -0300

    Better treatment for promotion

--- src/application/program.java
+++ src/application/program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -42,8 +43,12 @@ public class Program {
 				}
 				
 				if (chessMatch.getPromoted() != null) {
-					System.out.print("Enter piece for promotion (R/N/B/Q)");
-					String type=sc.nextLine();
+					System.out.print("Enter piece for promotion (R/N/B/Q): ");
+					String type=sc.nextLine().toUpperCase();
+					while (!type.equals("Q") && !type.equals("B") && !type.equals("N") && !type.equals("R")) {
+						System.out.print("Invalid value!\nEnter piece for promotion (R/N/B/Q): ");
+						type=sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 				
